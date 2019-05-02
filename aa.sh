0069b9c8750502498208a2040faef7034732dc00
bintangAlif5 <49284528+bintangAlif5@users.noreply.github.com>
2019-05-03 02:18:54 +0700

    Create aa.sh

---
+++ aa.sh
@@ -0,0 +1,18 @@
+ 
+echo "____________________________________\n";
+    echo "1). Microsoft Checker";
+    echo "2). paypal Checker Email. Valid";
+    echo "Note : Ini Manual Ajg, Otw Yg Starbuks( Chat Gua Aja Anjeng )";
+    echo "____________________________________";
+    echo "<---- Mau Yang Mana Njeng? ---->\n";
+    read -p "--# " star; 
+if [ $star = 1 ] || [ $star = 1 ]
+then
+sleep 2
+python2 st.py
+fi
+if [ $star = 2 ] || [ $star = 2 ]
+then
+sleep 2
+python2 t.py
+fi
