75fb8079ce425282a5d359c19bcc410524130ea6
bintangAlif5 <49284528+bintangAlif5@users.noreply.github.com>
2019-05-17 05:15:30 +0700

    Update aa.sh

--- aa.sh
+++ aa.sh
@@ -2,7 +2,9 @@
 echo "____________________________________\n";
     echo "1). Microsoft Checker";
     echo "2). paypal Checker Email. Valid";
-    echo "3). Starbuks ( Chat Gua Aja Anjeng )";
+    echo "3). Bin Checker Script ( Chat Gua Aja Anjeng )";
+    echo "4). Yahoo Checker";
+    echo "5). Tools Online kid ( To defacer )";
     echo "Note : Ini Manual Ajg Otw";
     echo "____________________________________";
     echo "<---- Mau Yang Mana Njeng? ---->\n";
@@ -17,3 +19,16 @@ then
 sleep 2
 python2 t.py
 fi
+if [ $star = 3 ] || [ $star = 3]
+then
+sleep2
+echo "Males Banget gua gituinnya :v ";
+echo "-->  https://arc.bukancoder.co/BIN-Checker/index.php.txt";
+echo "Noh itu dah gua kasih link scriptnya salin bae sendiri :v";
+fi
+if [ $star = 4 ] || [ $star = 4 ]
+then
+sleep2
+python2 yahoo.py
+fi
+if [ $star = 5 ] || [ $star = 6 ]
