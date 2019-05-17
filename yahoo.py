174263c82950ecf47c2b13c011e5fc7f9f98b839
bintangAlif5 <49284528+bintangAlif5@users.noreply.github.com>
2019-05-17 10:11:28 +0700

    Update yahoo.py

--- yahoo.py
+++ yahoo.py
@@ -8,6 +8,7 @@ yellow = '\033[33;1m'
 red = '\033[31;1m'
 purple = '\033[35;1m'
 
+os.system("clear")
 time.sleep(2)
 os.system('figlet Yahoo Checker')
 star = """
@@ -26,7 +27,7 @@ print "\033[93mMau Auto apa kgaa?\n 1). Ya\n 2). Tidak"
 putput = raw_input("input --> \033[95m")
 if putput == "1":
    gudd = raw_input("Masukan Nama Wordlistnya : ")
-   aerr = open("%s", "r")%(gudd)
+   aerr = open("'+gudd'", "r")
    ct = aerr.read().splitlines() 
    r = requests.get("http://widhitools.000webhostapp.com/api/yahoo.php?email="+ct)
    print green+"[!] "+blue+"Account : "+red, r.json()["email"]
